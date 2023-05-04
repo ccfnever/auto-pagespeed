781b34da0b66e8656a93d33251149ce7c25332a9
ccfnever <349090627@qq.com>
2023-05-04 14:18:58 +0800

    定义配置

---
+++ config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    reportName: 'tgame 预约站性能测试',
+    pc: [
+      'https://snowbreak.amazingseasun.com/m/en/index.html'
+    ],
+    mobile:[
+      'https://snowbreak.amazingseasun.com/m/en/index.html'
+    ],
+    tool: 'pagespeed',
+    count: 10,
+    sleepTime: 60,
+  }
+]
