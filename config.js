ee40b80d972a8fc3af88b20aa6343cba93966015
ccfnever <349090627@qq.com>
2023-05-15 17:17:21 +0800

    config test

--- config.js
+++ config.js
@@ -1,45 +1,25 @@
 export default [
   {
     // 输出的报告名称
-    reportName: 'tgame 预约站性能测试 PC',
+    reportName: 'xxxx 性能测试 PC',
 
     // pc 端测试地址
     url: [
-      'https://snowbreak.amazingseasun.com/en/index.html'
+      'https://www.bilibili.com'
     ],
 
     // 测试工具，pagespeed 则使用 psi 测试
     tool: 'pagespeed',
 
     // 测试次数
-    count: 5,
+    count: 10,
 
     // 单次测试后的间隔时间
-    sleepTime: 10,
+    sleepTime: 2,
 
     // 模式
     strategy: 'desktop'
   },
   
-  {
-    // 输出的报告名称
-    reportName: 'tgame 预约站性能测试 移动端',
-
-    // pc 端测试地址
-    url: [
-      'https://snowbreak.amazingseasun.com/en/index.html'
-    ],
-
-    // 测试工具，pagespeed 则使用 psi 测试
-    tool: 'pagespeed',
-
-    // 测试次数
-    count: 5,
-
-    // 单次测试后的间隔时间
-    sleepTime: 10,
-
-    // 模式
-    strategy: 'mobile'
-  },
+  
 ]
